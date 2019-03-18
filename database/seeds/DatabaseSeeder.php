90dfbbc98e8e268b536ee1b6c4eb7e80597250af
Rom <romdevmod@gmail.com>
2019-03-18 16:31:42 +0800

    added company & department seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,19 +14,11 @@ class DatabaseSeeder extends Seeder
 
 		$this->call(UsersTableSeeder::class);
 
-//    	factory(App\Company::class, 1)->create()->each(function($company) {
-//
-//    		$company->departments()->saveMany(factory(App\Department::class, 5)->make());
-//
-//    	});
-//
-//    	factory(App\User::class, 10)->create()->each(function($user) {
-//
-//    		$user->profile()->save(factory(App\Profile::class)->make());
-//    		$user->contacts()->saveMany(factory(App\Contact::class, 5)->make());
-//    		$user->employee()->save(factory(App\Employee::class)->make());
-//
-//    	});
+   	factory(App\Company::class, 1)->create()->each(function($company) {
+
+   		$company->departments()->saveMany(factory(App\Department::class, 5)->make());
+
+   	});
 
 
     }

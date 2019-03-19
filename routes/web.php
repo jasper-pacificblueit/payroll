802c12f71b17982bd996ee38c412cb5a77a4b587
Jude-sama <judeagagad@gmail.com>
2019-03-19 16:16:28 +0800

    update dtr

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::middleware(['auth'])->group(function () {
 
 
 	Route::resource('dtr', 'DateTimeRecordController');
+	Route::post('dtr/view' , 'DateTimeRecordController@records');
 
 
 	Route::post('/company/{company}/department' , 'DepartmentController@store');

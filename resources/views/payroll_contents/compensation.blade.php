6fc473a15163cf2c7bd6740fd4bb16e6b1438eeb
Denver <ahdeaq02@gmail.com>
2019-04-12 15:14:46 +0800

    search

--- resources/views/payroll_contents/compensation.blade.php
+++ resources/views/payroll_contents/compensation.blade.php
@@ -1,9 +1,8 @@
 <div class="row">
-    <div class="col-lg-12">
-        <div class="col-lg-3">
+    <div class="col-lg-3">
+        <h4>Payroll Records </h4>
             <div class="input-group"><input type="text" placeholder="Search" class="input-sm form-control"> <span class="input-group-btn">
-                <button type="button" class="btn btn-sm btn-success">Go!</button> </span></div>
-
+                <button type="button" class="btn btn-sm btn-success">Go!</button> </span>
         </div>
     </div>
 </div>

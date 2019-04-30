790487df9aae8313146ffcea68218938d0e51d90
Rom <romdevmod@gmail.com>
2019-04-30 13:24:44 +0800

    Tue 30 Apr 2019 01:24:43 PM PST

--- resources/views/employee_contents/manage_employee.blade.php
+++ resources/views/employee_contents/manage_employee.blade.php
@@ -9,9 +9,41 @@
                 <button type="button" style="padding:10px" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
                <h4 style="padding:10px"></h4>
             </div>
-            <form method="POST" action="/employee/{{ $user->id }}">
-                {{ csrf_field() }}
-                {{ method_field('put') }}
+            <form method="POST" action="/employee/{{ $user->id }}" onsubmit="
+
+                event.preventDefault();
+
+                var form = this;
+
+                fetch('/employee/{{ $user->id }}', {
+                    method: 'post',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                    },
+                    body: JSON.stringify((function() {
+
+                        var json = {};
+
+                        for (var i in form.elements)
+                            if (form.elements[i].name && form.elements[i].value)
+                                json[form.elements[i].name] = form.elements[i].value;
+                            else if (form.elements[i].name)
+                                json[form.elements[i].name] = form.elements[i].placeholder;
+
+                        return json;
+                    })()),
+
+                }).then(rep => {console.log(rep); return rep.text(); }).then(txt => {
+                    
+                    $('#manage #close').click();
+
+                    EmployeeSelect(document.querySelector('#DepartmentSelector').value);
+                });
+
+
+
+            ">
                 <div class="modal-body">
                    <div class="row">
                        <div class="col-lg-3 col-md-3 col-sm-5 col-xs-6">
@@ -23,9 +55,9 @@
                        <div class="col-lg-4 col-md-5 col-sm-4 col-xs-6">
                             <label>Department</label>
                             <br>
-                            <select class="form-control" style="width: 100%">
+                            <select class="form-control" name="department" style="width: 100%">
                                 @foreach(App\Company::find($eminfo->company_id)->departments as $dep)
-                                    <option value="{{ $dep->id }}">{{ $dep->name }}</option>
+                                    <option value="{{ $dep->id }}" {{ $eminfo->department_id == $dep->id ? 'selected' : '' }}>{{ $dep->name }}</option>
                                 @endforeach
                             </select>
                        </div>
@@ -33,7 +65,7 @@
                 </div>
                 <div class="modal-footer">
                     <div class='btn-group'>
-                        <button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-success" data-dismiss="modal" id='close'>Close</button>
                         <button type="submit" class="btn btn-success" name="submit">Save</button>
                     </div>
                 </div>
